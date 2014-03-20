086eb7b4c550751a540d65b9f14f5124b2740f16
Josh Wentz <joshwentz@gmail.com>
2014-03-19 21:57:59 -0400

    Bash script to install OpenStudio1.3 on Ubuntu 12.04

---
+++ install-openstudio.sh
@@ -0,0 +1,8 @@
+cd /usr/local/
+wget http://developer.eebhub.org/installers/OpenStudio-1.3.0.e09fcf487f-Nodejs-Linux.deb
+dpkg -i OpenStudio-1.3.0.e09fcf487f-Nodejs-Linux.deb
+sudo apt-get -f install
+sudo apt-get install -f
+dpkg -i OpenStudio-1.3.0.e09fcf487f-Nodejs-Linux.deb
+sudo apt-get install libgl1-mesa-glx -y
+sudo apt-get install xvfb
