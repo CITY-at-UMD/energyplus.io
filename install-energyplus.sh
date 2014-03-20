4922ee55e12bc90b0d937c282c3273f5b083b963
Josh Wentz <joshwentz@gmail.com>
2014-03-19 21:57:07 -0400

    Bash script to install EnergyPlus8.0 on Ubuntu 12.04

---
+++ install-energyplus.sh
@@ -0,0 +1,11 @@
+cd /usr/local/;
+sudo wget http://developer.nrel.gov/downloads/buildings/energyplus/builds/EPlusV800008-lin-64.tar.gz;
+sudo tar xzf EPlusV800008-lin-64.tar.gz;
+find EnergyPlus-8-0-0/EnergyPlus -type f -perm -o+rx;
+find EnergyPlus-8-0-0/ExpandObjects -type f -perm -o+rx;
+find EnergyPlus-8-0-0/EPMacro -type f -perm -o+rx;
+mv EnergyPlus-8-0-0 /usr/local/;
+cd /usr/local/bin/;
+sudo ln -s /usr/local/EnergyPlus-8-0-0/EnergyPlus;
+sudo ln -s /usr/local/EnergyPlus-8-0-0/ExpandObjects;
+sudo ln -s /usr/local/EnergyPlus-8-0-0/EPMacro;
