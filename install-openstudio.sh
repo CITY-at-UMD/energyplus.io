f6cd387ca8b68133660ab8b2112b92eb7ff0777f
Josh Wentz <joshwentz@gmail.com>
2014-03-20 01:23:58 -0400

    Add OpenStudio Node Modules to $PATH

--- install-openstudio.sh
+++ install-openstudio.sh
@@ -6,3 +6,11 @@ sudo apt-get install -f
 dpkg -i OpenStudio-1.3.0.e09fcf487f-Nodejs-Linux.deb
 sudo apt-get install libgl1-mesa-glx -y
 sudo apt-get install xvfb
+
+#ADD OPENSTUDIO NODE MODULES TO $PATH
+export NODE_PATH=/usr/local/lib/openstudio/node
+PATH=$PATH:/usr/local/lib/openstudio/node
+sudo echo “export NODE_PATH=/usr/local/lib/openstudio/node” >> /etc/profile
+sudo echo “export PATH=$PATH:$NODE_PATH” >> /etc/profile
+sudo echo “export NODE_PATH=/usr/local/lib/openstudio/node” >> /etc/environment
+sudo echo “export PATH=$PATH:$NODE_PATH” >> /etc/environment
