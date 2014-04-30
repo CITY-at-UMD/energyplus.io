18822f076a0336d9b504002530ca863e37c26666
Josh Wentz <joshwentz@gmail.com>
2014-04-30 08:42:01 -0400

    jQuery Client Events now inside HTML rather than separate javascript

--- public/displaylogs.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var socket = io.connect('http://localhost:9099');
-    var container = $('#container');
-    socket.on('new-data', function(data) {
-        var newItem = $('<div>' + data.value + '</div>');
-        container.append(newItem);
-    });
-});
